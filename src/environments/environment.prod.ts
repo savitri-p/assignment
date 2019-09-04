558ac180b7d86ea7ed8602013bef351d1fbeb3d5
unknown <savitri.p@gmail.com>
2019-09-05 02:56:03 +0700

    Config firebase api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebase: {
+    apiKey: "AIzaSyAYnPRv8E9Qd5M_rrEogoTDpnmEH3lNbbs",
+    authDomain: "inbox-component.firebaseapp.com",
+    databaseURL: "https://inbox-component.firebaseio.com",
+    projectId: "inbox-component",
+    storageBucket: "inbox-component.appspot.com",
+    messagingSenderId: "428127189238",
+    appId: "1:428127189238:web:5d19e0e25924e04f"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebase : {
+    apiKey: "AIzaSyAYnPRv8E9Qd5M_rrEogoTDpnmEH3lNbbs",
+    authDomain: "inbox-component.firebaseapp.com",
+    databaseURL: "https://inbox-component.firebaseio.com",
+    projectId: "inbox-component",
+    storageBucket: "inbox-component.appspot.com",
+    messagingSenderId: "428127189238",
+    appId: "1:428127189238:web:5d19e0e25924e04f"
+  }
 };
 
 /*
