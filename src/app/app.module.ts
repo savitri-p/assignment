b6d7fff632d0fd4687a42310a3d31444ef6910e6
unknown <savitri.p@gmail.com>
2019-09-05 02:55:24 +0700

    Add moment && firebase package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MomentModule } from 'ngx-moment';
+//import { AngularFireModule } from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InboxComponent } from './inbox/inbox.component';
 import { EmailsComponent } from './inbox/emails/emails.component';
 import { EmailComponent } from './inbox/email/email.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,9 @@ import { EmailComponent } from './inbox/email/email.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MomentModule
+    //AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
