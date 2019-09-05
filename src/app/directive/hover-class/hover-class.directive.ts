cf5421c408787514d37b3bc4a9b8354ac83c4e97
unknown <savitri.p@gmail.com>
2019-09-05 23:57:17 +0700

    remove eventData from mouseEnter and mouse leave

--- src/app/directive/hover-class/hover-class.directive.ts
+++ src/app/directive/hover-class/hover-class.directive.ts
@@ -14,14 +14,11 @@ export class HoverClassDirective {
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
-  @HostListener('mouseenter') onMouseEnter(eventData: Event) {
+  @HostListener('mouseenter') onMouseEnter() {
     this.renderer.addClass(this.elRef.nativeElement, this.hoverClass);
-    //this.renderer.setElementClass(this.elfRef, this.hoverClass, true);
-    //this.elRef.nativeElement.classList.add(this.hoverClass);
   }
 
-  @HostListener('mouseleave') onMouseLeave(eventData: Event) {
-    //this.renderer.setElementClass(this.elfRef, this.hoverClass, true);
+  @HostListener('mouseleave') onMouseLeave() {
     this.renderer.removeClass(this.elRef.nativeElement, this.hoverClass);
   }
 
