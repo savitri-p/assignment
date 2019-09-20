7b7156681b63f93c682ad59301085218ad14bc84
savitri-p <savitri@digitalendpoint.com>
2019-09-20 14:16:57 +0700

    Add loadedEmail && loadedEmailChanged Subject to firebase service and emails component

--- src/app/inbox/emails/emails.component.ts
+++ src/app/inbox/emails/emails.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FirebaseService } from '../firebase.service';
 import { Email } from '../models/Email.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-emails',
   templateUrl: './emails.component.html',
   styleUrls: ['./emails.component.scss']
 })
-export class EmailsComponent implements OnInit {
+export class EmailsComponent implements OnInit, OnDestroy{
   emails:Email[];
   error = null;
+  subscription: Subscription;
 
   constructor(private firebaseService: FirebaseService) { }
 
   ngOnInit() {
+    this.subscription = this.firebaseService.loadedEmailChanged
+      .subscribe(
+        (emails: Email[]) => {
+          this.emails = emails;
+        }
+      )
+
     this.firebaseService.fetchEmails().subscribe(emails => {
       this.emails = emails;
     },
@@ -21,5 +30,9 @@ export class EmailsComponent implements OnInit {
       this.error = "Something went wrong, please try again later.";
     });      
   } 
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
 }
--- src/app/inbox/firebase.service.ts
+++ src/app/inbox/firebase.service.ts
@@ -1,20 +1,27 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable  } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { Email } from './models/Email.model';
 
-@Injectable({
+@Injectable({ // [metadata] expect to get sth injected.
   providedIn: 'root'
 })
-export class FirebaseService {
+
+export class FirebaseService  {
+  loadedEmails: Email[] = [];
+  loadedEmailChanged = new Subject<Email[]>();
+
   emailUrl:string = "https://programming-challenge-30fe8.firebaseio.com/emails.json";
 
   constructor(private http:HttpClient) { }
-
-  fetchEmails():Observable<Email[]> {
-    return this.http.get<Email[]>(this.emailUrl);
+ 
+  fetchEmails():Observable<Email[]> { 
+    return this.http.get<Email[]>(this.emailUrl)
+      .pipe(
+        map(responseData => this.loadedEmails = responseData)
+      );      
   }
   
 }
